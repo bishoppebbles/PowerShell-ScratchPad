54bed637a38e51bc1a9b670e26c636c2813bef65
Sam Pursglove <spursglove@gmail.com>
2021-06-21 11:46:23 +0800

    Added code to remove a local user profile

--- CIMStuff.ps1
+++ CIMStuff.ps1
@@ -1,8 +1,11 @@
-#View the Class definitions in a Namespace
+# View the Class definitions in a Namespace
 Get-CimClass
-Get-CimClass –ClassName <name> | Select-Object –ExpandProperties <properties>
+Get-CimClass â€“ClassName <name> | Select-Object â€“ExpandProperties <properties>
 
 
-#Check the local accounts on remote computer
-Get-CimInstance -ClassName Win32_UserAccount -ComputerName <computerName> -Filter "LocalAccount='True'" | Format-Table Caption,Disabled –AutoSize
+# Check the local accounts on remote computer
+Get-CimInstance -ClassName Win32_UserAccount -ComputerName <computerName> -Filter "LocalAccount='True'" | Format-Table Caption,Disabled â€“AutoSize
 Get-WmiObject -ClassName Win32_UserAccount -ComputerName <computerName> -Filter "LocalAccount='True'" | Format-Table Caption,Disabled -AutoSize
+
+# Remove a local user profile named "user_name"
+Get-CimInstance -ClassName Win32_UserProfile | Where-Object {$_.LocalPath('\')[-1] -eq 'user_name'} | Remove-CimInstance
