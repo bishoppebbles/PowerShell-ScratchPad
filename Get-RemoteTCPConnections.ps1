4a2b5ce85537116d9a53496d39bc24d81aa08bc2
Sam Pursglove <spursglove@gmail.com>
2022-06-08 21:39:12 -0400

    Initial commit

---
+++ Get-RemoteTCPConnections.ps1
@@ -0,0 +1,29 @@
+﻿# run every $interval seconds for New-TimeSpan length
+# to run infinitely change loop to while(1 -eq 1)
+
+$interval = 30
+$timer = New-TimeSpan -Minutes 60
+$clock = [diagnostics.stopwatch]::StartNew()
+
+
+function netConnects() {
+    $hashtable = @{}
+    $date = Get-Date -Format "MM/dd/yyyy"
+    $time = Get-Date -Format "HH:mm"
+
+        
+    Get-Process | 
+        ForEach-Object { 
+            $hashtable.$($_.Id) = $_.ProcessName
+        }
+
+    Get-NetTCPConnection |
+        Select-Object @{Name = "Date"; Expression = {$date}},@{Name = "Time"; Expression = {$time}},LocalAddress,LocalPort,RemoteAddress,RemotePort,State,OwningProcess,@{Name = "ProcessName"; Expression = {$hashtable[[int]$_.OwningProcess]}} |
+}
+
+while($clock.elapsed -lt $timer){
+#while(1 -eq 1) {
+    Invoke-Command -ComputerName <system> -ScriptBlock ${function:netConnects} |
+        Export-Csv -Path .\output.csv -Append -NoTypeInformation
+    Start-Sleep -Seconds $interval
+}
