b4c9ea59fb046dfc630607269d5619fa63d76157
Sam Pursglove <spursglove@gmail.com>
2022-09-22 12:11:53 +0900

    Converted to explicit ps sessions

--- CollectDomainData.ps1
+++ CollectDomainData.ps1
@@ -2,36 +2,37 @@
 $computers = (Get-ADComputer -Filter *).Name
 
 # Minimize your presence and don't create a user profile on every system (e.g., C:\Users\<username>)
-$sess = New-PSSessionOption -NoMachineProfile
+$sessionOpt = New-PSSessionOption -NoMachineProfile
+
+# Create reusable PS Sessions
+$sessions = New-PSSession -ComputerName $computers -SessionOption $sessionOpt
 
 # Local Administrators group membership
-Invoke-Command -ComputerName $computers -ScriptBlock {Get-LocalGroupMember Administrators} -SessionOption $sess | 
+Invoke-Command -Session $sessions -ScriptBlock {Get-LocalGroupMember Administrators} | 
 	Export-Csv -Path local_admins_group.csv -NoTypeInformation
 
 # Local user accounts
-Invoke-Command -ComputerName $computers -ScriptBlock {Get-LocalUser} -SessionOption $sess | 
+Invoke-Command -Session $sessions -ScriptBlock {Get-LocalUser} | 
 	Export-Csv -Path local_users.csv -NoTypeInformation
 
 # Processes
-Invoke-Command -ComputerName $computers -ScriptBlock {Get-Process | Select-Object Name,Id,Path,Company,Description,ProductVersion,StartTime} -SessionOption $sess |
+Invoke-Command -Session $sessions -ScriptBlock {Get-Process | Select-Object Name,Id,Path,Company,Description,ProductVersion,StartTime} |
 	Export-Csv -Path processes.csv -NoTypeInformation
 
 # Scheduled tasks
-Invoke-Command -ComputerName $computers -ScriptBlock {Get-ScheduledTask} -SessionOption $sess | 
+Invoke-Command -Session $sessions -ScriptBlock {Get-ScheduledTask} | 
 	Export-Csv -Path scheduled_tasks.csv -NoTypeInformation
 
 # Services
-Invoke-Command -ComputerName $computers -ScriptBlock {Get-Service} -SessionOption $sess | 
+Invoke-Command -Session $sessions -ScriptBlock {Get-Service} | 
 	Export-Csv -Path services.csv -NoTypeInformation
 
 # Downloads, Documents, and Desktop files
-Invoke-Command -ComputerName $computers -ScriptBlock {Get-ChildItem -Path 'C:\Users\*\Downloads\','C:\Users\*\Documents\','C:\Users\*\Desktop\' -Recurse} -SessionOption $sess | 
-	Select-Object Name,Extension,Directory,CreationTime,LastAccessTime,LastWriteTime,Attributes} |
+Invoke-Command -Session $sessions -ScriptBlock {Get-ChildItem -Path 'C:\Users\*\Downloads\','C:\Users\*\Documents\','C:\Users\*\Desktop\' -Recurse | Select-Object Name,Extension,Directory,CreationTime,LastAccessTime,LastWriteTime,Attributes} |
 	Export-Csv -Path files.csv -NoTypeInformation
 
 # 32/64 bit Programs
-Invoke-Command -ComputerName $computers -ScriptBlock {Get-ChildItem -Path 'C:\Program Files','C:\Program Files (x86)'} -SessionOption $sess | 
-	Select-Object Name,Directory,CreationTime,LastAccessTime,LastWriteTime,Attributes} -SessionOption $sess |
+Invoke-Command -Session $sessions -ScriptBlock {Get-ChildItem -Path 'C:\Program Files','C:\Program Files (x86)' | Select-Object Name,Directory,CreationTime,LastAccessTime,LastWriteTime,Attributes} |
 	Export-Csv -Path programs.csv -NoTypeInformation
 
 # Network connections
@@ -50,5 +51,7 @@ function netConnects() {
         Select-Object @{Name = "Date"; Expression = {$date}},@{Name = "Time"; Expression = {$time}},LocalAddress,LocalPort,RemoteAddress,RemotePort,State,OwningProcess,@{Name = "ProcessName"; Expression = {$hashtable[[int]$_.OwningProcess]}}
 }
 
-Invoke-Command -ComputerName $computers -ScriptBlock ${function:netConnects} -SessionOption $sess |
+Invoke-Command -Session $sessions -ScriptBlock ${function:netConnects} |
     Export-Csv -Path .\net.csv -Append -NoTypeInformation
+    
+Remove-PSSession -Session $sessions
