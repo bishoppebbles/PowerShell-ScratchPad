3a0643ee80ec2fc05ac39bb7463c7f80515e54a9
Sam Pursglove <spursglove@gmail.com>
2023-09-19 15:21:37 +0900

    Update IPAddressRegex.ps1

--- IPAddressRegex.ps1
+++ IPAddressRegex.ps1
@@ -1,8 +1,11 @@
-# input your block of IPs (e.g., from nmap)
+# copy/paste your block of IPs (e.g., from nmap)
 $ips = ‘<ip_list>’
 
+# or input them from a text file
+$ips = Get-Content <ips.txt>
+
 # parse the IPs and HTTP/S browse to each one with Chrome
-Start-Process chrome ((Select-String -InputObject $ips -Pattern '\d{1,3}(\.\d{1,3}){3}' -AllMatches).Matches.Value)
+Start-Process chrome ((Select-String -InputObject $ips -Pattern '\d{1,3}(\.\d{1,3}){3}' -AllMatches).Matches.Value  | Select-Object -Unique)
 
 # DNS lookup your block of IPs
-(Select-String -InputObject $ips -Pattern '\d{1,3}(\.\d{1,3}){3}' -AllMatches).Matches.Value | Resolve-DnsName
+(Select-String -InputObject $ips -Pattern '\d{1,3}(\.\d{1,3}){3}' -AllMatches).Matches.Value | Select-Object -Unique | Resolve-DnsName
