f9c6df47bee7a38b02b7a95136bb2d8987e12bdd
Sam Pursglove <spursglove@gmail.com>
2025-05-27 16:47:15 +0500

    Counts and groups Windows event log IDs with the description

---
+++ Group_Windows_Event_IDs.ps1
@@ -0,0 +1,6 @@
+﻿Get-WinEvent -Path <path.evtx> | 
+    Select-Object Id,TaskDisplayName | 
+    Group-Object Id | 
+    Sort-Object Count -Descending | 
+    Select-Object Count,Name,@{name='Description'; expression={$_.Group[0].TaskDisplayName}} | 
+    Export-Csv win_events.csv -NoTypeInformation
