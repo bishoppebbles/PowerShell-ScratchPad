7af312fbe3e3c98c7b2de2b59ebdcd86f83d48b0
Sam Pursglove <spursglove@gmail.com>
2025-07-17 11:20:08 +0545

    Added LDAP user and computer query code

---
+++ LDAP_computers.ps1
@@ -0,0 +1,64 @@
+# Populate list of workstations
+$computers = @()
+$Name = 'site'
+$Domain = 'domain.com'
+$OUdistinguishedName = 'OU=location,DC=domain,DC=com'
+$searcher =  [adsiSearcher]"(&(samAccountType=805306369)(attribute=$value))"
+$searcher.SearchRoot = [ADSI]"LDAP://$Domain/$OUdistinguishedname"
+$computers = $searcher.FindAll()
+
+[ScriptBlock]$Enable_PSRemoting = {
+    param (
+    	[Parameter(Mandatory = $true)] [string]$computer
+    )
+
+    $cimSessOption = New-CimSessionOption -Protocol Dcom
+
+    if(Test-Connection $computer -Count 1) {          
+        $cimSession = New-CimSession -ComputerName $computer -SessionOption $cimSessOption
+        Invoke-CimMethod -ClassName 'Win32_Process' `
+                         -MethodName 'Create' `
+                         -CimSession $cimSession `
+                         -Arguments @{CommandLine = "powershell Start-Process powershell -ArgumentList 'Enable-PSRemoting -Force'"} | Out-Null
+        $cimSession | Remove-CimSession
+
+        if(Test-WSMan -ComputerName $computer) {
+            Write-Output "PS Remoting was enabled on $computer"
+        } else {
+            Write-Output "PS Remoting was not enabled on $computer"
+        }
+    } else {
+        Write-Output "$computer is not reachable"
+    }
+}
+
+[ScriptBlock]$Enable_WinRM = {
+    param (
+        [Parameter(Mandatory = $true)] [string]$computer
+    )
+
+    sc.exe \\$computer config winrm start=auto
+    sc.exe \\$computer start winrm
+    #sc.exe \\$computer query winrm
+}
+
+$jobBatchSize = 50
+$i = 0
+$startTime = Get-Date
+
+foreach($computer in $computers) {
+    $i++
+    Start-Job -ScriptBlock $Enable_PSRemoting -ArgumentList $computer
+    #Start-Job -ScriptBlock $Enable_WinRM -ArgumentList $computer
+
+    # Run jobs in batches, also ensure the final batch of jobs is run if the total number of jobs is less than $jobBatchSize
+    if($i % $jobBatchSize -eq 0 -or $i -eq ($Computers | Measure-Object).Count) {
+        Get-Job | Wait-Job -Timeout 90
+        Get-Job | Receive-Job   # supresses the job output
+        Get-Job | Stop-Job      # must be run first to remove a job if the job if it is still running
+        Get-Job | Remove-Job
+    }
+}
+
+$elapsedTime = ($(Get-Date) - $startTime).ToString("hh\:mm\:ss")
+Write-Output "`Processed $(($computers | Measure-Object).Count) workstations in $elapsedTime."
---
+++ LDAP_users.ps1
@@ -0,0 +1,36 @@
+﻿$list = Get-Content .\users_list.txt
+Import-Module ActiveDirectory -ErrorAction SilentlyContinue
+
+# use the AD PowerShell module if available otherwise query based on LDAP
+if(Get-Module ActiveDirectory) {
+    $output = foreach($entry in $list) {
+        $escaped = $entry -replace "'","''"
+        $info = Get-ADUser -Filter "DisplayName -like '$escaped'" -Server state.sbu -Properties DisplayName,Title,Department,Description
+        [PSCustomObject]@{
+            DisplayName    = $info.DisplayName
+            Title          = $info.Title
+            Department     = $info.Department
+            Description    = $info.Description
+            SamAccountName = $info.SamAccountName
+        }
+    } 
+} else {
+    $Domain = 'domain.com'
+    $OUDistinguishedName = 'OU=location,DC=domain,DC=com'
+
+    $output = foreach($entry in $list) {
+        $searcher = [adsiSearcher]"(&(samAccountType=805306368)(DisplayName=$entry))"
+        $searcher.SearchRoot = [ADSI]"LDAP://$Domain/$OUDistinguishedName"
+        $info = $searcher.FindOne()
+
+        [PSCustomObject]@{
+            DisplayName    = $($info.Properties.displayname)
+            Title          = $($info.Properties.title)
+            Department     = $($info.Properties.department)
+            Description    = $($info.Properties.description)
+            SamAccountName = $($info.Properties.samaccountname)
+        }
+    }
+}
+
+$output | Export-Csv output.csv -NoTypeInformation
